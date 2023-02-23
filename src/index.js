79fdc65890c73fa3f152f18593b8ce77b56fb51f
filip mertens <filip.mertens@ethereum.org>
2023-02-23 07:54:29 +0100

    fix theme changes

--- src/index.js
+++ src/index.js
@@ -13,6 +13,8 @@ import $ from "jquery";
 var path = require('path')
 import 'bootstrap';
 
+let currentTheme = null
+
 
 class WorkSpacePlugin extends PluginClient {
 
@@ -21,8 +23,18 @@ class WorkSpacePlugin extends PluginClient {
         console.log("loading plugin backup zip restore")
         createClient(this);
         this.onload().then(async (x) => {
-            console.log("plugin restore backup zip loaded")
+            console.log("plugin restore backup zip loaded", this)
+            this.call('theme', 'currentTheme').then((theme) => {
+                currentTheme = theme
+                this.on("theme", "themeChanged", function (theme) {
+                    // reload the page to apply the theme
+                    if (currentTheme.name !== theme.name)
+                        window.location.reload();
+                })
+            })
+
         });
+
     }
 }
 /* globals zip, document, URL, MouseEvent, alert */
@@ -42,18 +54,18 @@ $(function () {
         files.forEach(render)
 
         function render(value, index, array) {
-            
+
             if (value.filename.indexOf('.workspaces/') > -1) {
-               const paths = value.filename.split(path.sep).filter((x) => x != '')
-               if(paths.length === 2){
-                let icon = ``
-                if(!value.directory)
-                    icon = `<span class='fas fa-upload pr-3'></span>`
-                else
-                    icon = `<span class="fas fa-folder-plus pr-3"></span>`
-                $("#file-list").append(`<li class=''><div  data-id='${index}' class='btn btn-primary mb-1 importfile'>import</div> ${paths[1]}</li>`)
-                //console.log(value.filename)
-               }
+                const paths = value.filename.split(path.sep).filter((x) => x != '')
+                if (paths.length === 2) {
+                    let icon = ``
+                    if (!value.directory)
+                        icon = `<span class='fas fa-upload pr-3'></span>`
+                    else
+                        icon = `<span class="fas fa-folder-plus pr-3"></span>`
+                    $("#file-list").append(`<li class=''><div  data-id='${index}' class='btn btn-primary mb-1 importfile'>import</div> ${paths[1]}</li>`)
+                    //console.log(value.filename)
+                }
             }
         }
         //console.log("done")
@@ -69,34 +81,34 @@ $(function () {
         if (!workspace) workspace = 'Undefined'
         let counter = ''
         let exist = true
-    
-        do {
-          const isDuplicate = workspaces.indexOf(workspace + counter) > -1
-    
-          if (isDuplicate) counter = (counter | 0) + 1
-          else exist = false
-        } while (exist)
-    
-        return workspace + counter
-      }
 
-    const importfile = async function(value){
+        do {
+            const isDuplicate = workspaces.indexOf(workspace + counter) > -1
+
+            if (isDuplicate) counter = (counter | 0) + 1
+            else exist = false
+        } while (exist)
+
+        return workspace + counter
+    }
+
+    const importfile = async function (value) {
         // console.log(value)
         $("#log").show()
         const workspace = value.filename.split(path.sep).filter((x) => x != '')[1]
-        const workspaces = await client.call('filePanel','getWorkspaces')
+        const workspaces = await client.call('filePanel', 'getWorkspaces')
         const worskspacetocreate = createNonClashingDirNameAsync(workspace, workspaces)
         try {
             await client.call('filePanel', 'createWorkspace', worskspacetocreate, true)
-            for(const ob of files){
+            for (const ob of files) {
                 const paths = ob.filename.split(path.sep).filter((x) => x != '')
-                if(paths[1] && paths[1] === workspace && paths.length > 2){
+                if (paths[1] && paths[1] === workspace && paths.length > 2) {
                     // console.log(workspace, paths, ob)
                     const finalpath = paths.slice(2).join("/")
                     if (ob.directory) {
                         try {
-                           await client.call("fileManager", "mkdir", finalpath)
-                           $("#log-entry").append(`<li>imported ${finalpath}  into ${worskspacetocreate}</>`)
+                            await client.call("fileManager", "mkdir", finalpath)
+                            $("#log-entry").append(`<li>imported ${finalpath}  into ${worskspacetocreate}</>`)
                         } catch (e) {
                             $("#log-entry").append(`<li class='text-danger'>${e}</>`)
                         }
@@ -113,11 +125,10 @@ $(function () {
                     }
                 }
             }
-        }catch(e) {
+        } catch (e) {
             $("#log-entry").append(`<li class='text-danger'>${e}</>`)
         }
 
-        await client.call('fileManager', 'refresh')
         return
     }
 
